740d21d2d6f4dd244ca44c5fa0b261ac1f77afd4
Josh Date <jdate@98point6.com>
2019-05-29 22:00:26 -0700

    fancy headers based on scrolling

--- main.js
+++ main.js
@@ -11,16 +11,56 @@ function parseHash() {
     if (activeElement ) {
       activeElement.classList.add('active-page');
       activeElement.querySelector('a').setAttribute('disabled', true);
+      document.querySelector(hash).scrollIntoView();
     }
   }
 }
 
 
-window.addEventListener('hashchange', function() {
+window.addEventListener('hashchange', () => {
   parseHash();
   
 }, false);
 
+function scrollListener() {
+  const currentActive = document.querySelector('.header-nav-list-item.active-page > a');
+  let lastHash = currentActive && currentActive.getAttribute('href');
+
+  const navMenu = document.querySelector('.header-nav');
+  const navMenuHeight = navMenu.offsetHeight;
+
+  const navLinks = navMenu.querySelectorAll('.header-nav-list-item a');
+
+  const scrollItems = [...navLinks].map(navLink => {
+    return navLink.getAttribute('href');
+  })
+
+  document.querySelector('main').addEventListener('scroll', (event) => {
+    const fromTop = document.querySelector('main').scrollTop;
+
+    const nextScrollHeaderIndex = scrollItems.findIndex(item => {
+      const $el = document.querySelector(item);
+      const yOffset = $el.getBoundingClientRect()['y'];
+
+      return $el.getBoundingClientRect()['y'] >= 52;
+    })
+
+    const currentIndex = nextScrollHeaderIndex > -1 ? nextScrollHeaderIndex - 1 : scrollItems.length - 1;
+    const currentHash = scrollItems[currentIndex]
+
+    if (currentHash !== lastHash) {
+      lastHash = currentHash
+
+      const currentlyActive = document.querySelector('.header-nav-list-item.active-page');
+      currentlyActive && currentlyActive.classList.remove('active-page');
+      const activeName = currentHash.substring(1);
+      const activeElement = document.querySelector(`.header-nav-list-item.${activeName}`);
+      activeElement.classList.add('active-page');
+      activeElement.querySelector('a').setAttribute('disabled', true);
+    }
+  })
+}
 
 
 parseHash();
+scrollListener();
